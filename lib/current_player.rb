bfdda14489a5bc909334e2f77c8622ad096a666a
Michael Bade <mbade314@gmail.com>
2020-05-08 16:07:44 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,10 @@ def turn_count(board)
   #counter variable
   counter = 0
   #board each method
-  board.each |space|
+  board.each do |space|
+    if space == "X" || space == "O"
+      counter += 1
+    else 
     
     #if var in board == "X" or "O"
       #counter += 1
